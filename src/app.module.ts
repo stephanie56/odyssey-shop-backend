3ce4aa9f2f2c1473342860e48f499bf8b1cb75cb
Stephanie Zeng <stephanie.zeng@rangle.io>
2019-12-19 00:16:54 -0500

    #6 Add TypeORM and configure mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from 'config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    /** Load and parse .env files from the environments directory */
+    ConfigModule.forRoot({
+      envFilePath: 'src/environment/*.env',
+      load: [databaseConfig],
+      isGlobal: true,
+    }),
+    /** Configure TypeOrm asynchronously. */
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) =>
+        configService.get('database'),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
